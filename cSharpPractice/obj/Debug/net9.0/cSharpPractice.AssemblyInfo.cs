1a65be52509ed21a71d705f067118b4b5a86995a
maxainsworth <maxainsworth5@gmail.com>
2025-02-01 12:28:27 +0000

    test

--- cSharpPractice/obj/Debug/net9.0/cSharpPractice.AssemblyInfo.cs
+++ cSharpPractice/obj/Debug/net9.0/cSharpPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1933c5da00d73c134bfeec366b82d35610eb0c70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e22aa89786d410d7ee8455aea3993151f328df8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
